f67ae33f1040c065514bf3812919fce2a8bb501a
Ellen Nitchals <ellen.nitchals@gmail.com>
2024-02-15 11:15:19 -0800

    initial code for todo app

---
+++ src/ToDoList.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function ToDoList() {
+
+  return (
+    <>
+      <h2>My To Do List</h2>
+      <ul>
+        <li>Teach React</li>
+        <li>React Demo</li>
+      </ul>
+    </>
+  )
+}
+
+export default ToDoList
